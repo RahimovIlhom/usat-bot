0b167ac368240f599ce82c8f8dc314cc023cba23
RahimovIlhom <ilhomjonrahimov217@gmail.com>
2024-05-21 12:05:57 +0500

    optimal and clean code text execution

--- states/test_states.py
+++ states/test_states.py
@@ -2,6 +2,4 @@ from aiogram.dispatcher.filters.state import StatesGroup, State
 
 
 class TestExecutionStates(StatesGroup):
-    science1 = State()
-    science2 = State()
-    science3 = State()
+    science = State()
